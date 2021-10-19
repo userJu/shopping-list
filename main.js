a9d8e2e280852fc58c39832e8b3ce1af76274950
JuhyunLim <87933367+userJu@users.noreply.github.com>
2021-10-19 18:07:15 +0900

    JS: finished

--- main.js
+++ main.js
@@ -5,9 +5,56 @@ const addBtn = document.querySelector('.footer__button')
 function onAdd(){
     // 1. 사용자가 입력한 txt를 받아옴
     const text = input.value;
-    console.log(text)
+    if(text === ''){
+        input.focus();
+        return;
+    }
     // 2. 받아온 txt를 이용해서 새로운 아이템을 만듦(텍스트 + 삭제)
+    const item = createItem(text);
     // 3. items 컨테이너안에 새로 만든 아이템을 추가한다
+    items.appendChild(item)
+    // + 새로 추가된 아이템으로 스크롤링
+    item.scrollIntoView({block:'center'})
     // 4. input을 초기화시킨다
+    input.value="";
+    input.focus();
 }
-onAdd()
+
+function createItem(text){
+    const itemRow = document.createElement('li');
+    itemRow.setAttribute('class', 'item__row')
+
+    const item = document.createElement('div');
+    item.setAttribute('class', 'item')
+
+    const span = document.createElement('span');
+    span.setAttribute('class', 'item__name');
+    span.innerText = text;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('class', 'item__delete');
+    deleteBtn.innerHTML=`<i class="fas fa-trash-alt"></i>`
+    deleteBtn.addEventListener('click', ()=>{
+        items.removeChild(itemRow)
+    })
+
+    const itemDivider = document.createElement('div');
+    itemDivider.setAttribute('class', 'item__divider');
+
+    itemRow.appendChild(item)
+    item.appendChild(span)
+    item.appendChild(deleteBtn)
+    itemRow.appendChild(itemDivider)
+    return itemRow;
+
+}
+
+addBtn.addEventListener('click', ()=>{
+    onAdd();
+})
+
+input.addEventListener('keypress',(event)=>{
+    if(event.key === 'Enter'){
+        onAdd();
+    }
+} )
