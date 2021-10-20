f325b136210521370ace1c0fa68ac3d04500ae6f
JuhyunLim <87933367+userJu@users.noreply.github.com>
2021-10-20 13:56:52 +0900

    js: event delegation
    teacher's coad
    내가 innerHTML로 list를 만들었을 때 문제가 생겼던 이유는 삭제를 못해서.
    그 문제는 delete를 밖으로 빼고 이벤트 위임을 통한 이벤트를 이용해 해결할 수 있었다.
    
    조금 더 큰 관점에서 바라보자.

--- main1.js
+++ main1.js
@@ -14,32 +14,46 @@ function manageItem(){
     
 }
 
+let id = 0 // UUID같은 고유한 ID를 쓰거나 오브젝트에 있는 해시 코드를 이용해서 고유하게 만드는게 좋긴 하다. 배포레벨이 아니기 때문에 간단하게 숫자 변수로 시행
 function loadItem(value){
     const li = document.createElement('li')
     li.setAttribute('class','item__row')
+    li.setAttribute('data-id',id)
+    li.innerHTML = `
+            <div class="item">
+                <span class="item__name">${value}</span>
+                <button class="item__delete"><i class="fas fa-trash-alt" data-id =${id}></i></button>
+            </div>
+            <div class="item__divider"></div>
+            `
+        id++;
+        
+    items.appendChild(li);
 
-    const div = document.createElement('div')
-    div.setAttribute('class','item')
 
-    const span = document.createElement('span')
-    span.setAttribute('class','item__name')
-    span.innerHTML=value;
 
-    const deleteBtn = document.createElement('button')
-    deleteBtn.setAttribute('class','item__delete')
-    deleteBtn.innerHTML=`<i class="fas fa-trash-alt"></i>`
-    deleteBtn.addEventListener('click', ()=>{
-        li.remove()
-    })
+    // const div = document.createElement('div')
+    // div.setAttribute('class','item')
 
-    const divider = document.createElement('div')
-    divider.setAttribute('class','item__divider')
+    // const span = document.createElement('span')
+    // span.setAttribute('class','item__name')
+    // span.innerHTML=value;
 
-    items.appendChild(li)
-    li.appendChild(div)
-    div.appendChild(span)
-    div.appendChild(deleteBtn)
-    li.appendChild(divider)
+    // const deleteBtn = document.createElement('button')
+    // deleteBtn.setAttribute('class','item__delete')
+    // deleteBtn.innerHTML=`<i class="fas fa-trash-alt"></i>`
+    // deleteBtn.addEventListener('click', ()=>{
+    //     li.remove()
+    // })
+
+    // const divider = document.createElement('div')
+    // divider.setAttribute('class','item__divider')
+
+    // items.appendChild(li)
+    // li.appendChild(div)
+    // div.appendChild(span)
+    // div.appendChild(deleteBtn)
+    // li.appendChild(divider)
     
 }
 
@@ -57,3 +71,10 @@ footerBtn.addEventListener('click', ()=>{
 
 })
 
+items.addEventListener('click', (e)=>{
+    const id = e.target.dataset.id
+    if(id){
+        const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`)
+        toBeDeleted.remove();
+    }
+})
